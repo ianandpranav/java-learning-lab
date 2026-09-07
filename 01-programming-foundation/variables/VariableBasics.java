7f6ee2b03800ad181aa6ba2da774cce1707122a7
Anand Pranav <71137592+ianandpranav@users.noreply.github.com.>
2026-09-07 23:43:10 +0530

    feat: add variable basics example

---
+++ 01-programming-foundation/variables/VariableBasics.java
@@ -0,0 +1,21 @@
+public class VariableBasics {
+
+    public static void main(String[] args) {
+
+        // Variable declaration and initialization
+        int age = 21;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Anand";
+
+        // Reassignment
+        age = 22;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
