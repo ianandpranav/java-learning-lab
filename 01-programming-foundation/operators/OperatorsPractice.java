fcc325b7eea1252d1b79768cd573ba8624f8df9f
Anand Pranav <71137592+ianandpranav@users.noreply.github.com>
2026-09-11 23:31:39 +0530

    Add OperatorsPractice class to calculate percentage

---
+++ 01-programming-foundation/operators/OperatorsPractice.java
@@ -0,0 +1,17 @@
+public class OperatorsPractice {
+
+    public static void main(String[] args) {
+
+        int marks = 78;
+        int totalMarks = 100;
+
+        double percentage = (marks * 100.0) / totalMarks;
+
+        System.out.println("Marks: " + marks);
+        System.out.println("Percentage: " + percentage);
+
+        boolean passed = marks >= 40;
+
+        System.out.println("Passed: " + passed);
+    }
+}
