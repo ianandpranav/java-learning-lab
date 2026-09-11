58df64257a35bffc7e62c7f46397fb769737a82f
Anand Pranav <71137592+ianandpranav@users.noreply.github.com>
2026-09-12 01:37:48 +0530

    feat: add Student class for OOP basics

---
+++ 02-oop/classes-objects/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+    // State / Properties
+    String name;
+    int age;
+    String course;
+
+    // Behavior / Method
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
